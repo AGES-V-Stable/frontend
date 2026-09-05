7a515fe539947396b4c7492696a9ba1411eab0a6
juliofi <juliopontocom2@gmail.com>
2026-09-04 21:49:32 -0300

    mudar tamanho da label

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,7 +3,7 @@ import type { InputType } from './InputType'
 function Input({ label, placeholder, value, onChange }: InputType) {
     return (
         <div className="flex flex-col gap-y-1 w-full">
-            <p>{label}</p>
+            <p className="text-[14px] text-[#3C4A42]">{label}</p>
             <input type="text" placeholder={placeholder} value={value} onChange={onChange} className="w-full py-3.5 px-3 text-[16px] border border-[#BBCABF] rounded-lg bg-[#F8F9FB]" />
         </div>
     )
