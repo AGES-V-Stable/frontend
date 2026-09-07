195eaf00cc7f5bea25a4b12295539b9be41f37d8
juliofi <juliopontocom2@gmail.com>
2026-09-07 19:26:55 -0300

    testes

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,8 @@ export default defineConfig({
         'src/setupTests.ts',
         'src/**/*.test.{ts,tsx}',
         'src/**/*.spec.{ts,tsx}',
+        'src/**/index.ts',
+        'src/**/*Type.ts',
       ],
       thresholds: {
         lines: 80,
