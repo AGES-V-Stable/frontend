d14ff6699b45ebe53b69b45672134be2163c2ce2
juliofi <juliopontocom2@gmail.com>
2026-09-07 18:17:49 -0300

    mudar a cor do placeholder

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,7 +4,7 @@ function Input({ label, placeholder, value, onChange }: InputType) {
     return (
         <div className="flex flex-col gap-y-1 w-full">
             <p className="text-[14px] text-[#3C4A42]">{label}</p>
-            <input type="text" placeholder={placeholder} value={value} onChange={onChange} className="w-full py-3.5 px-3 text-[16px] border border-[#BBCABF] rounded-lg bg-[#F8F9FB]" />
+            <input type="text" placeholder={placeholder} value={value} onChange={onChange} className="w-full py-3.5 px-3 text-[16px] placeholder:text-[#6B7280] border border-[#BBCABF] rounded-lg bg-[#F8F9FB]" />
         </div>
     )
 }
