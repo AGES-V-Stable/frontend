761a16eca62e0243f82752b0863feebee901ce24
juliofi <juliopontocom2@gmail.com>
2026-09-07 18:47:50 -0300

    resolver mais problemas de lint

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,10 +1,8 @@
-import { Input } from "@/components/Input"
-
 function Register() {
   return <>
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
+      Register
+
     </div>
   </>
 }
