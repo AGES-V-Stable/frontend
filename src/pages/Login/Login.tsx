cfc38918719e8ff1979687f0dcf0d6ebebc17eee
juliofi <juliopontocom2@gmail.com>
2026-09-07 21:07:52 -0300

    estrutura

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,9 @@
 function Login() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-        <h1>Login</h1>
+      <div className="flex items-center min-h-screen">
+        <div className="bg-[#0F172A] min-h-screen w-[760px] flex items-center justify-center">div da esquerda azul escuro</div>
+        <div className="bg-[#FFFFFF] flex items-center justify-center min-h-screen w-full">div da direita cinza</div>
       </div>
     </>
   )
